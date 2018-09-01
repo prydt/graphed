662504a1f56ac1a19b0a6cd0e42d76fa63e5004a
prydt <pranoydll@gmail.com>
2018-08-31 20:52:46 -0500

    completed scraper!

--- scraper.py
+++ scraper.py
@@ -1,11 +1,13 @@
 import requests # for a good HTTP client
 from bs4 import BeautifulSoup # for parsing soup
 
+import pprint
+
 URL = 'https://hac.friscoisd.org/HomeAccess'
 
 def getData(username, password):
 
-    data = { 'classes': []}
+    data = { 'classes': [] }
 
     s = requests.Session()
 
@@ -24,23 +26,46 @@ def getData(username, password):
 
     classes = soup.find_all(class_='AssignmentClass')
 
+    avgnum = 0 # for getting averages
+
     for c in classes:
         # single class object
-        obj = { assignments: []}
+        obj = { 'assignments': [] }
 
         # eldritch horror that makes class names prettier
         obj['name'] = c.find('a', class_='sg-header-heading').string.split('-')[1][2:].strip()
 
-        # TODO add average grade for class
+        # average grade for class
+        obj['average'] = float(c.find('span', id='plnMain_rptAssigmnetsByCourse_lblHdrAverage_{}'.format(avgnum)).string[14:-1])
+        avgnum = avgnum + 1
 
-        rows = c.find_all(class_='sg-asp-table-data-row')
+        rows = c.find('table', class_='sg-asp-table').find_all(class_='sg-asp-table-data-row')
 
-        # TODO add assignments to class
-        for r in rows:
+        # add assignments to class
+        for row in rows:
             assignment = {}
-            #assignment[]
+
+            # filling out assignment attributes
+            columns = row.find_all('td')
+            assignment['date_due'] = columns[0].string.strip()
+            assignment['date_assigned'] = columns[1].string.strip()
+            assignment['name'] = columns[2].find('a').string.strip()
+            assignment['category'] = columns[3].string.strip()
+
+            # CAN be None, so check
+            if len(columns[4].string.strip()) > 0:
+                assignment['score'] = float(columns[4].string.strip('\n %'))
+            else:
+                assignment['score'] = None
+
+            if len(columns[5].string.strip()) > 0:
+                assignment['total_points'] = float(columns[5].string.strip('\n %'))
+            else:
+                assignment['total_points'] = None
+
+            obj['assignments'].append(assignment)
 
         # adding class to collection of classes
         data['classes'].append(obj)
 
-    print(data)
+    pprint.pprint(data)
--- server.py
+++ server.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python3
 import scraper
+import os
 
 if __name__ == '__main__':
     # TODO add server interface
+
+    # this is just for testing
+    scraper.getData(os.environ['HUSERNAME'], os.environ['HPASSWORD'])
